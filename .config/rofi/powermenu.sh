920df9d84bb487f253da41344f1503ba999f04f7
dfbk1 <>
2024-11-30 23:58:15 +0200

    added rofi config

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,40 @@
+#!/bin/env bash
+
+# Options for powermenu
+logout="󰍃 Logout"
+shutdown="⏻ Shutdown"
+reboot=" Reboot"
+sleep="  Sleep"
+
+# Get answer from user via rofi
+selected_option=$(echo "$logout
+$sleep
+$reboot
+$shutdown" | rofi -dmenu\
+                  -i\
+                  -p "Power"\
+                  -width "15"\
+                  -lines 5\
+                  -line-margin 3\
+                  -line-padding 10\
+                  -scrollbar-width "0" )
+
+# Do something based on selected option
+echo "$selected_option"
+if [ "$selected_option" == "$logout" ]
+then
+    loginctl terminate-user `whoami`
+elif [ "$selected_option" == "$shutdown" ]
+then
+    systemctl poweroff
+elif [ "$selected_option" == "$reboot" ]
+then
+    systemctl reboot
+elif [ "$selected_option" == "$sleep" ]
+then
+    systemctl suspend
+else
+    echo "No match"
+fi
+
+
